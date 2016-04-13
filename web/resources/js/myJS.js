64ae5ccbb4627c5c02c7000b0c8858b95ab8b387
amel <amel@ami>
2016-04-13 14:27:14 +0200

    v 1.0 Prvo izdanje

--- web/resources/js/myJS.js
+++ web/resources/js/myJS.js
@@ -45,7 +45,16 @@ function GreskaProzor(){
 	if(myTextField.value != ""){		
              document.getElementById("modal").style.display="block";
          }
-
-    
+}
+function w3_openMeni() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementsByClassName("w3-sidenav")[0].style.width = "25%";
+  document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
+  document.getElementsByClassName("w3-opennav")[0].style.display = 'none';
+}
+function w3_closeMeni() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
+  document.getElementsByClassName("w3-opennav")[0].style.display = "inline-block";
 }
 

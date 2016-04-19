a9fb79a3851653cc2d4dac78206cd5a4d58d6f96
amel <amel@ami>
2016-04-19 14:06:57 +0200

    v 1.0 Prvo izdanje - rješio print - malo uljepšao kod - dodao pretragu

---
+++ src/java/Login/Korisnik.java
@@ -0,0 +1,27 @@
+package Login;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+
+/**
+ *
+ * @author amel
+ */
+@SessionScoped
+@ManagedBean 
+public class Korisnik {
+
+    public Korisnik() {
+    }
+    
+    @PostConstruct
+    public void init() {
+    
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
